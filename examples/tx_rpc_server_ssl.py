edbd876c7c9d3d61821d5d69aec5b283a98c7929
Jakub Matys <jakub.matys@nodus.cz>
2015-01-29 15:24:17 +0100

    Added examples of both server and client that use SSL

---
+++ examples/tx_rpc_server_ssl.py
@@ -0,0 +1,22 @@
+from twisted.internet import defer, reactor, ssl
+from txmsgpackrpc.server import MsgpackRPCServer
+
+
+class EchoRPC(MsgpackRPCServer):
+
+    def remote_echo(self, value, msgid=None):
+        return defer.succeed(value)
+
+
+def main():
+    server = EchoRPC()
+
+    sslContext = ssl.DefaultOpenSSLContextFactory(
+                    'examples/cert/example.key',
+                    'examples/cert/example.cert')
+
+    reactor.listenSSL(8000, server.factory, sslContext)
+
+if __name__ == '__main__':
+    reactor.callWhenRunning(main)
+    reactor.run()
