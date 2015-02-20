4e449ac08fa270eac4b90732a7b0f164195db76c
Jakub Matys <jakub.matys@nodus.cz>
2015-02-20 01:54:58 +0100

    Prepared to deploy to pypi.python.org

--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
-import distutils.core
+import os.path
+import setuptools
+
 
 __VERSION__ = "0.2"
 
-distutils.core.setup(
+
+# Hard linking doesn't work inside some virtual FS's.
+try:
+    setup_path = os.path.abspath(__file__)
+    linked_path = setup_path + '.lnk'
+    os.link(setup_path, linked_path)
+    os.unlink(linked_path)
+except OSError as e:
+    import errno
+    if e.errno == errno.EPERM:
+        del os.link
+
+
+with open("README.md") as readme:
+    long_description = readme.read()
+
+
+setuptools.setup(
     name = "txmsgpackrpc",
     version = __VERSION__,
     packages = ["txmsgpackrpc"],
@@ -11,4 +30,23 @@ distutils.core.setup(
     url = "https://github.com/jakm/txmsgpackrpc",
     license = "MIT",
     description = "txmsgpackrpc is a Twisted library to support msgpack-rpc",
+    long_description=long_description,
+    install_requires = [
+        "Twisted>=14.0",
+        "msgpack-python>=0.4",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Twisted",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords = "twisted msgpack rpc",
 )
